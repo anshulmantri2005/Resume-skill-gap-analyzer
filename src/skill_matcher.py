fe3cc3140a7fc34f00e8904d1a6ae131330b8507
aashnashahi <143892664+aashnashahi@users.noreply.github.com>
2026-05-07 14:18:00 +0530

    committed

--- src/skill_matcher.py
+++ src/skill_matcher.py
@@ -1,3 +1,9 @@
+from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+
 def match_skills(resume_skills, job_skills):
 
     matched = []
@@ -14,3 +20,16 @@ def match_skills(resume_skills, job_skills):
     score = (len(matched) / len(job_skills)) * 100
 
     return matched, missing, score
+
+def semantic_match(resume_text, job_text):
+
+    resume_embedding = model.encode([resume_text])
+    job_embedding = model.encode([job_text])
+
+    similarity = cosine_similarity(
+        resume_embedding,
+        job_embedding
+    )[0][0]
+
+    return round(similarity * 100, 2)
+    
